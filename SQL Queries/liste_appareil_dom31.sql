9f65be427b011faa8933d64175c24958929f87fa
Colibridev <54209311+Fifaliana-tech@users.noreply.github.com>
2022-06-18 01:35:39 +0300

    queries

---
+++ SQL Queries/liste_appareil_dom31.sql
@@ -0,0 +1,2 @@
+SELECT nom_appareil, nom_pièce FROM jarvis.appareils, jarvis.piece
+WHERE piece.id_Pièce = appareils.Pièce_id_Pièce AND piece.Domicile_id_Domicile = 31;
---
+++ SQL Queries/liste_pièces_dom_id11.sql
@@ -0,0 +1,2 @@
+SELECT * FROM jarvis.piece
+WHERE Domicile_id_Domicile = 11
